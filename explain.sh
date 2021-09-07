ef60185db5bc2775aeb84f522782ba86cc7107b1
MJR <m.j.rahman@outlook.com>
2021-09-07 21:54:05 +0100

    trail

---
+++ explain.sh
@@ -0,0 +1,18 @@
+
+FirstNameOfWitness="Annabel" 
+LastNameOfWitness="Church"
+InterviewNumber="699607"
+Car="Blue Honda"
+FirstNameOfSuspect1="Hellen" 
+LastNameOfSuspect1="Maher"
+FirstNameOfSuspect2="Joe" 
+LastNameOfSuspect2="Germuska"
+FirstNameOfSuspect3="Heather"
+LastNameOfSuspect3="Billings"
+
+printf "$FirstNameOfWitness $LastNameOfWitness\n"
+printf "$InterviewNumber\n"
+printf "$Car\n"
+printf "$FirstNameOfSuspect1 $LastNameOfSuspect1\n"
+printf "$FirstNameOfSuspect2 $LastNameOfSuspect2\n"
+printf "$FirstNameOfSuspect3 $LastNameOfSuspect3\n"
