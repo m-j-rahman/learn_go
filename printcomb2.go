f5e69576c77e8707949ab58c0974150ab8690e8e
MJR <m.j.rahman@outlook.com>
2021-09-08 15:51:16 +0100

    for if else

--- printcomb2.go
+++ printcomb2.go
@@ -5,10 +5,10 @@ import (
 )
 
 func PrintComb2() {
-	for a := '0'; a < '9'; a++ {
-		for b := '0'; b < '9'; b++ {
-			for c := '0'; c < '9'; c++ {
-				for d := '0'; d < '9'; d++ {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
 					if c > a || c == a && d > b {
 
 						z01.PrintRune(a)
