8d848f38a096491369ddbb2a2a9c7e6dd9fc76b0
MJR <m.j.rahman@outlook.com>
2021-09-09 16:57:03 +0100

    for len

---
+++ strrev.go
@@ -0,0 +1,13 @@
+package piscine
+
+func StrRev(s string) string {
+	a := []rune(s)
+
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+
+	{
+		return string(a)
+	}
+}
