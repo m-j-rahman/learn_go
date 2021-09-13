4b22d447ae63a2ad4fcd9cc5825dd1a3a576b0aa
MJR <m.j.rahman@outlook.com>
2021-09-13 19:16:30 +0100

    len

--- nrune.go
+++ nrune.go
@@ -1,9 +1,21 @@
-package piscine
+package main
+
+import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
 	strArr := []rune(s)
 	if n < 0 || n > (len(strArr)) {
 		return 0
+	} else {
+		return rune(strArr[n-1])
 	}
-	return rune(strArr[n-1])
+}
+
+func main() {
+	z01.PrintRune(NRune("Hello!", 3))
+	z01.PrintRune(NRune("Salut!", 2))
+	z01.PrintRune(NRune("Bye!", -1))
+	z01.PrintRune(NRune("Bye!", 3))
+	z01.PrintRune(NRune("Ola!", 8))
+	z01.PrintRune('\n')
 }
