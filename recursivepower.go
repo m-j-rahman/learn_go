5f36f24cf94dade61fb42d1a0b345017bf6c7131
MJR <m.j.rahman@outlook.com>
2021-09-13 16:26:43 +0100

    recursive

--- recursivepower.go
+++ recursivepower.go
@@ -1,13 +1,11 @@
 package piscine
 
-import "fmt"
-
 func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
-	} else if power < 1 
+	} else if power < 1 {
 		return 1
 	} else {
 		return nb * RecursivePower(nb, power-1)
 	}
-
+}
