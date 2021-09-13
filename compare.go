2cc0cd368ab38e3503c2b77850f87b909e43bfbb
MJR <m.j.rahman@outlook.com>
2021-09-13 19:35:38 +0100

    if else

---
+++ compare.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Compare(a, b string) int {
+	if a == b {
+		return 0
+	} else if a < b {
+		return -1
+	} else {
+		return 1
+	}
+}
+
+func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}
