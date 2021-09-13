52b2f0b38d2f3a142db007f217199a03797312e8
MJR <m.j.rahman@outlook.com>
2021-09-13 15:39:20 +0100

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,15 +1,23 @@
-package piscine
+package main
+
+import (
+	"fmt"
+)
 
 func IterativeFactorial(nb int) int {
 	result := 1
 
-	if nb < 0 {
+	if nb < 0 || nb > 24 {
 		return 0
+	} else if nb >= 0 && nb <= 24 {
+		for i := 1; i < nb+1; i++ {
+			result = result * i
+		}
 	}
-
-	for i := 1; i < nb+1; i++ {
-		result = result * i
-	}
-
 	return result
+}
+
+func main() {
+	arg := 25
+	fmt.Println(IterativeFactorial(arg))
 }
