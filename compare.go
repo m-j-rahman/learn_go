e67c7b03f39863638b2ec45fd09013336f6dd60d
MJR <m.j.rahman@outlook.com>
2021-09-13 19:35:58 +0100

    if else

--- compare.go
+++ compare.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func Compare(a, b string) int {
 	if a == b {
@@ -10,10 +8,4 @@ func Compare(a, b string) int {
 	} else {
 		return 1
 	}
-}
-
-func main() {
-	fmt.Println(Compare("Hello!", "Hello!"))
-	fmt.Println(Compare("Salut!", "lut!"))
-	fmt.Println(Compare("Ola!", "Ol"))
 }
