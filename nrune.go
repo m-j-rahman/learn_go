0279d17fab40f125e57eaa243bc07d9acd5cfb57
MJR <m.j.rahman@outlook.com>
2021-09-13 19:26:03 +0100

    len

--- nrune.go
+++ nrune.go
@@ -2,7 +2,7 @@ package piscine
 
 func NRune(s string, n int) rune {
 	strArr := []rune(s)
-	if n < 0 || n > (len(strArr)) {
+	if n <= 0 || n > (len(strArr)) {
 		return 0
 	} else {
 		return strArr[n-1]
