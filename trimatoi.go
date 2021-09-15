42305fe098db81c422bbf8e10a90d3126af83b16
MJR <m.j.rahman@outlook.com>
2021-09-15 16:28:23 +0100

    aaaaa

--- trimatoi.go
+++ trimatoi.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func TrimAtoi(s string) int {
 	neg := false
@@ -20,15 +18,4 @@ func TrimAtoi(s string) int {
 		return trim * -1
 	}
 	return trim
-}
-
-func main() {
-	fmt.Println(TrimAtoi("12345"))
-	fmt.Println(TrimAtoi("str123ing45"))
-	fmt.Println(TrimAtoi("012 345"))
-	fmt.Println(TrimAtoi("Hello World!"))
-	fmt.Println(TrimAtoi("sd+x1fa2W3s4"))
-	fmt.Println(TrimAtoi("sd-x1fa2W3s4"))
-	fmt.Println(TrimAtoi("sdx1-fa2W3s4"))
-	fmt.Println(TrimAtoi("sdx1+fa2W3s4"))
 }
