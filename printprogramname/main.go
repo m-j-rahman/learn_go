b64dcab1b3f9cbe94e15e6e871b80b84aacd3327
MJR <m.j.rahman@outlook.com>
2021-09-16 15:16:39 +0100

    q6

---
+++ printprogramname/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := []rune(os.Args[0])
+	var index int
+	for i := len(argument) - 1; i > 0; i-- {
+		if argument[i] == '/' {
+			index = 1
+			break
+		}
+	}
+	for i := index - 1; i < len(argument); i++ {
+		z01.PrintRune(argument[i])
+	}
+	z01.PrintRune('\n')
+}
