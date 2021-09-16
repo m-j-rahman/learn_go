2ee8352cb540ca98a7445390c93eb4dcfa5cbaf7
MJR <m.j.rahman@outlook.com>
2021-09-16 15:38:40 +0100

    q6

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -11,11 +11,11 @@ func main() {
 	var index int
 	for i := len(argument) - 1; i > 0; i-- {
 		if argument[i] == '/' {
-			index = 2
+			index = 1
 			break
 		}
 	}
-	for i := index - 1; i < len(argument); i++ {
+	for i := index + 1; i < len(argument); i++ {
 		z01.PrintRune(argument[i])
 	}
 	z01.PrintRune('\n')
