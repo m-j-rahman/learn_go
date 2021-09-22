f3fcb6a73a80c3821fdb59ee994ca1d3fca05994
MJR <m.j.rahman@outlook.com>
2021-09-22 20:05:07 +0100

    loop

--- abort.go
+++ abort.go
@@ -2,11 +2,9 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	array := []int{a, b, c, d, e}
-	for i := 0; i <= 4; i++ {
-		for j := 1; j < len(array); j++ {
-			if array[j] < array[j-1] {
-				array[j], array[j-1] = array[j-1], array[j]
-			}
+	for j := 1; j < len(array); j++ {
+		if array[j] < array[j-1] {
+			array[j], array[j-1] = array[j-1], array[j]
 		}
 	}
 	return array[2]
